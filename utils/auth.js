65d03d00e85f7b58a699dfb2054772d0d9baa648
joeng03 <yinjoeng2003@gmail.com>
2023-11-07 02:12:02 +0800

    fix: uncomment login code

--- utils/auth.js
+++ utils/auth.js
@@ -10,10 +10,10 @@ const withAuth = (Component) => {
       return <p>Loading...</p>;
     }
 
-    // if (!user) {
-    //   return <Redirect ssr to="/api/auth/login" />;
-    // }
-    // console.log("role is false!")
+    if (!user) {
+      return <Redirect ssr to="/api/auth/login" />;
+    }
+    console.log("role is false!");
     return <Component user={user} isLoading={isLoading} {...props} />;
   };
 };
