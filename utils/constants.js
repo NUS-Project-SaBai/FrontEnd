4871d5a1eb8802b652c10ababe903b068068ce9a
joeng03 <yinjoeng2003@gmail.com>
2023-11-07 03:52:20 +0800

    Change localhost API endpoint to production endpoint

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,2 @@
-// export const API_URL = "https://projectsabai-vza8.onrender.com";
+export const API_URL = "https://projectsabai-vza8.onrender.com";
 export const CLOUDINARY_URL = "https://res.cloudinary.com/dxy0byphl";
-export const API_URL = "http://localhost:8000";
