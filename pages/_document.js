0439af37bdf4ee6a58ea0b96c7f0352122498414
keith <keithonghx@gmail.com>
2024-01-29 18:16:05 +0800

    removed default scrollbars

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" style={{ overflowY: "hidden" }}>
+    <Html lang="en" style={{ overflowY: "auto" }}>
       <Head></Head>
       <body>
         <Main />
