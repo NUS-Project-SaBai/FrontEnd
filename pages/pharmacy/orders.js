3094832a10f35f09c79efe6ab2b13ae040aaa2a4
davidgohzk <david.gohzhekai@gmail.com>
2024-02-07 21:43:20 +0800

    changed a little

--- pages/pharmacy/orders.js
+++ pages/pharmacy/orders.js
@@ -1,17 +1,14 @@
-// code is around 75% similar to that of queue.js
-// there is definitely room for refactoring
-
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API_URL, CLOUDINARY_URL } from '../../utils/constants';
-import withAuth from '../../utils/auth';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { API_URL, CLOUDINARY_URL } from "@/utils/constants";
+import withAuth from "@/utils/auth";
 
 const Orders = () => {
   const [visits, setVisits] = useState([]);
   const [visitsFiltered, setVisitsFiltered] = useState([]);
   const [orders, setOrders] = useState([]);
-  const [filteredVisitIdsUsingOrders, setFilteredVisitIdsUsingOrders] = useState(new Set());
+  const [filteredVisitIdsUsingOrders, setFilteredVisitIdsUsingOrders] =
+    useState(new Set());
 
   useEffect(() => {
     fetchOrdersAndVisits();
@@ -19,10 +16,14 @@ const Orders = () => {
 
   const fetchOrdersAndVisits = async () => {
     try {
-      const { data: orders } = await axios.get(`${API_URL}/orders?order_status=PENDING`);
+      const { data: orders } = await axios.get(
+        `${API_URL}/orders?order_status=PENDING`
+      );
       const { data: visits } = await axios.get(`${API_URL}/visits`);
-      
-      const newFilteredVisitIds = new Set(orders.map(order => order.consult.visit.id));
+
+      const newFilteredVisitIds = new Set(
+        orders.map((order) => order.consult.visit.id)
+      );
       setFilteredVisitIdsUsingOrders(newFilteredVisitIds);
       setVisits(visits);
       setVisitsFiltered(visits);
@@ -33,9 +34,12 @@ const Orders = () => {
   };
 
   const onFilterChange = (event) => {
-    const filteredVisits = visits.filter(visit => {
-      const patientId = `${visit.patient.village_prefix}${visit.patient.id}`.padStart(3, '0').toLowerCase() +
-                        ` ${visit.patient.name} ${visit.patient.local_name}`.toLowerCase();
+    const filteredVisits = visits.filter((visit) => {
+      const patientId =
+        `${visit.patient.village_prefix}${visit.patient.id}`
+          .padStart(3, "0")
+          .toLowerCase() +
+        ` ${visit.patient.name} ${visit.patient.local_name}`.toLowerCase();
       return patientId.includes(event.target.value.toLowerCase());
     });
     setVisitsFiltered(filteredVisits);
@@ -44,35 +48,42 @@ const Orders = () => {
   const handlePrescriptionAction = async (prescriptions, actionType) => {
     if (window.confirm(`Are you sure you want to ${actionType} this order?`)) {
       try {
-        const promises = prescriptions.map(prescription => {
+        const promises = prescriptions.map((prescription) => {
           return axios.patch(`${API_URL}/orders/${prescription.id}`, {
-            order_status: actionType === 'approve' ? 'approved' : 'cancelled'
+            order_status: actionType === "approve" ? "approved" : "cancelled",
           });
         });
         await Promise.all(promises);
         fetchOrdersAndVisits();
       } catch (error) {
-        console.error('Error updating orders:', error);
+        console.error("Error updating orders:", error);
       }
     }
   };
 
   const renderTableContent = () => {
     return visitsFiltered
-      .filter(visit => filteredVisitIdsUsingOrders.has(visit.id))
-      .map(visit => {
-        const Id = `${visit.patient.village_prefix}${visit.patient.id}`.padStart(3, '0');
+      .filter((visit) => filteredVisitIdsUsingOrders.has(visit.id))
+      .map((visit) => {
+        const Id =
+          `${visit.patient.village_prefix}${visit.patient.id}`.padStart(3, "0");
         const imageUrl = `${CLOUDINARY_URL}/${visit.patient.picture}`;
         const fullName = visit.patient.name;
 
-        const correctPrescription = orders.filter(order => order.consult.visit.id === visit.id);
+        const correctPrescription = orders.filter(
+          (order) => order.consult.visit.id === visit.id
+        );
         // i changed this to only show notes if it exist, else will just omit
-        const prescriptions = correctPrescription.map(prescription => (
+        const prescriptions = correctPrescription.map((prescription) => (
           <li key={prescription.id}>
-            {prescription.medicine?.medicine_name || ''}: {prescription.quantity}<br />
+            {prescription.medicine?.medicine_name || ""}:{" "}
+            {prescription.quantity}
+            <br />
             {prescription.notes && (
               <>
-                Notes: {prescription.notes}<br /><br />
+                Notes: {prescription.notes}
+                <br />
+                <br />
               </>
             )}
           </li>
@@ -83,16 +94,33 @@ const Orders = () => {
             <td>{Id}</td>
             <td>
               <figure className="image is-96x96">
-                <img src={imageUrl} alt="Patient" style={{ height: 96, width: 96, objectFit: 'cover' }} />
+                <img
+                  src={imageUrl}
+                  alt="Patient"
+                  style={{ height: 96, width: 96, objectFit: "cover" }}
+                />
               </figure>
             </td>
             <td>{fullName}</td>
-            <td><ul>{prescriptions}</ul></td>
             <td>
-              <button className="button is-dark" onClick={() => handlePrescriptionAction(correctPrescription, 'approve')}>
+              <ul>{prescriptions}</ul>
+            </td>
+            <td>
+              <button
+                className="button is-dark"
+                onClick={() =>
+                  handlePrescriptionAction(correctPrescription, "approve")
+                }
+              >
                 Prescribe
               </button>
-              <button className="button is-danger" style={{ marginLeft: '10px' }} onClick={() => handlePrescriptionAction(correctPrescription, 'cancel')}>
+              <button
+                className="button is-danger"
+                style={{ marginLeft: "10px" }}
+                onClick={() =>
+                  handlePrescriptionAction(correctPrescription, "cancel")
+                }
+              >
                 Cancel
               </button>
             </td>
@@ -104,7 +132,9 @@ const Orders = () => {
   return (
     <div style={{ marginTop: 15, marginLeft: 25, marginRight: 25 }}>
       <div className="column is-12">
-        <h1 style={{ color: 'black', fontSize: '1.5em' }}>Approve/Reject Orders</h1>
+        <h1 style={{ color: "black", fontSize: "1.5em" }}>
+          Approve/Reject Orders
+        </h1>
         <div className="field">
           <div className="control">
             <input
@@ -115,7 +145,7 @@ const Orders = () => {
             />
           </div>
         </div>
-          <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+        <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
           <thead>
             <tr>
               <th>ID</th>
