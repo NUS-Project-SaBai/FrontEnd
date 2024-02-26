5a60b5fb03fc0af1b55eb15627c506a96cf747f7
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-02-26 18:18:04 +0800

    undo changes in constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
-//export const API_URL = "https://projectsabai-vza8.onrender.com";
+export const API_URL = "https://projectsabai-vza8.onrender.com";
 export const CLOUDINARY_URL = "https://res.cloudinary.com/dxy0byphl";
-export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000";
 
 export const venueOptions = {
   SV: "Smong Village",
