3caf400dd807d3248e4b923dc1ab45c6c5cebe85
davidgohzk <david.gohzhekai@gmail.com>
2024-05-08 12:55:54 +0800

    refactored registration

--- pages/registration/PatientModal.js
+++ pages/registration/PatientModal.js
@@ -1,8 +1,32 @@
 import Modal from "react-modal";
-import VenueOptions from "./VenueOptions";
 import { Button } from "@/components/textContainers/Button";
 import { InputField } from "@/components/textContainers/InputField";
 import { InputBox } from "@/components/textContainers/InputBox";
+import { venueOptions } from "@/utils/constants";
+
+const VenueOptions = ({ handleInputChange }) => (
+  <div>
+    <label
+      htmlFor="Village"
+      className="block text-xs font-medium text-gray-900"
+    >
+      Village
+    </label>
+
+    <select
+      className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+      name="village_prefix"
+      onChange={handleInputChange}
+      default={Object.keys(venueOptions)[0]}
+    >
+      {Object.entries(venueOptions).map(([key, value]) => (
+        <option value={key} key={value}>
+          {value}
+        </option>
+      ))}{" "}
+    </select>
+  </div>
+);
 
 const PatientModal = ({
   modalIsOpen,
