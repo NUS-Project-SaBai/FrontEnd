967a657af1d25796ad6eed7aa0b73e14f9a719e1
taggyhan123@Gmail.com <taggyhan123@Gmail.com>
2024-06-29 21:57:01 +0800

    Introduced prettier, eslint

--- components/Loading.js
+++ components/Loading.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Loading() {
   return (
-    <div className='flex flex-col items-center h-full my-auto'>
-        <h1 className='text-[50px]'>Loading....</h1>
+    <div className="flex flex-col items-center h-full my-auto">
+      <h1 className="text-[50px]">Loading....</h1>
     </div>
-  )
+  );
 }
 
-export default Loading
+export default Loading;
