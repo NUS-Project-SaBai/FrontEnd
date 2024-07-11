5916df0c1b5e9d9f37cdc2790a64bb1e3181683c
davidgohzk <david.gohzhekai@gmail.com>
2024-07-11 12:04:08 +0800

    updated with the new village codes

--- utils/constants.js
+++ utils/constants.js
@@ -1,16 +1,15 @@
 // export const API_URL = "https://projectsabai-vza8.onrender.com";
-export const CLOUDINARY_URL = "https://res.cloudinary.com/dlusodadw";
-export const API_URL = "http://localhost:8000";
+export const CLOUDINARY_URL = 'https://res.cloudinary.com/dlusodadw';
+export const API_URL = 'http://localhost:8000';
 
 export const venueOptions = {
-  PP: "Phnom Penh",
-  PTC: "PTC",
-  TT: "TT",
-  CATT: "CATT",
-  PC: "PC",
-  SV: "Smong Village",
+  PC: 'PC',
+  CA: 'CA',
+  TT: 'TT',
+  TK: 'TK',
+  SV: 'Smong',
 };
 
-export const NO_PHOTO_MESSAGE = "Please take a photo before submitting!";
-export const NO_MATCHES_FOUND_MESSAGE = "No matches found!";
-export const MATCH_FOUND_MESSAGE = "Match found!";
+export const NO_PHOTO_MESSAGE = 'Please take a photo before submitting!';
+export const NO_MATCHES_FOUND_MESSAGE = 'No matches found!';
+export const MATCH_FOUND_MESSAGE = 'Match found!';
