88f8324bd7ffd73d1725bf20b844f214c1cb2908
davidgohzk <david.gohzhekai@gmail.com>
2024-08-13 00:57:30 +0800

    removed redundant code

--- pages/_app.js
+++ pages/_app.js
@@ -6,23 +6,10 @@ import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 import Layout from '@/components/layout';
 import '@/styles/globals.css';
-import { OFFLINE } from '@/utils/constants';
 
 Modal.setAppElement('#__next');
 
 const MyApp = ({ Component, pageProps }) => {
-  // const offline_user = window.localStorage.getItem('offline_user');
-  // const offline_user = null;
-  const [offlineUser, setOfflineUser] = useState(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const offlineUserFromStorage =
-        window.localStorage.getItem('offline_user');
-      setOfflineUser(offlineUserFromStorage);
-    }
-  }, []);
-
   return (
     <UserProvider>
       <Layout>
