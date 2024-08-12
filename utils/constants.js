c78ec967de9b55387cb50ea09fe3f93edcfc6ecf
davidgohzk <david.gohzhekai@gmail.com>
2024-08-12 17:36:10 +0800

    added code to add in the api url if the api url has not been set

--- utils/constants.js
+++ utils/constants.js
@@ -4,15 +4,22 @@ export const CLOUDINARY_URL = 'https://res.cloudinary.com/dlusodadw';
 
 export const defaultAPI_URL = process.env.API_URL || 'http://localhost:8000';
 
-const windowCheck = typeof window === "undefined";
+const isWindowUndefined = typeof window === 'undefined';
 
-export const getAPI_URL = () => (!windowCheck)
-  ? window.localStorage.getItem('API_URL')
-  : defaultAPI_URL;
+export const getAPI_URL = () => {
+  if (isWindowUndefined) {
+    return defaultAPI_URL;
+  } else {
+    if (window.localStorage.getItem('API_URL') === null) {
+      window.localStorage.setItem('API_URL', defaultAPI_URL);
+    }
+    return window.localStorage.getItem('API_URL');
+  }
+};
 
-export const changeAPI_URL = (newURL) => {
+export const changeAPI_URL = newURL => {
   window.localStorage.setItem('API_URL', newURL);
-}
+};
 
 export const venueOptions = {
   PC: 'PC',
