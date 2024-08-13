fd793c7842ed3a713f323f65c77859014c5389bc
keithxun <keithonghx@gmail.com>
2024-08-13 17:51:49 +0800

    fix: loading widget animation

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@ const Loading = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-75 z-50">
       <div
-        className="spinner-border animate-spin inline-block w-8 h-8 border-4 border-blue-500 rounded-full"
+        className="animate-spin rounded-full h-8 w-8 border-t-4 border-b-4 border-blue-500"
         role="status"
       >
         <span className="sr-only">Loading...</span>
