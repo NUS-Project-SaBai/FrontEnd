53bbed4e2f342f421db9e29644f3f4a88008d02c
Ang Peng Xuan <ang.peng.xuan@gmail.com>
2024-08-19 01:42:38 +0800

    add back locations

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -59,7 +59,11 @@ const navigation = [
   },
 ];
 
-const locations = [];
+const locations = [
+  { id: 1, name: 'Heroicons', href: '#', initial: 'H', current: false },
+  { id: 2, name: 'Tailwind Labs', href: '#', initial: 'T', current: false },
+  { id: 3, name: 'Workcation', href: '#', initial: 'W', current: false },
+];
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
