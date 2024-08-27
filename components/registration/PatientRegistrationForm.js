1b3d35b5f556d0427ae0de8ab5e5b7901f295e35
keithxun <keithonghx@gmail.com>
2024-08-27 23:21:26 +0800

    Standardise names

--- components/registration/PatientRegistrationForm.js
+++ components/registration/PatientRegistrationForm.js
@@ -4,7 +4,7 @@ import AppWebcam from '@/components/WebCamera';
 import React, { useState } from 'react';
 
 export const VenueOptions = ({ handleInputChange }) => {
-  const handleChange = event => {
+  const handleChangeWithStyle = event => {
     const selectedValue = event.target.value;
     event.target.className = `flex-1 block w-full rounded-md border-2 py-2 px-1.5 bg-white focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6 ${
       villageColorClasses[selectedValue] || 'text-orange-500'
@@ -24,7 +24,7 @@ export const VenueOptions = ({ handleInputChange }) => {
         <select
           name="village_prefix"
           id="village_prefix"
-          onChange={handleChange}
+          onChange={handleChangeWithStyle}
           defaultValue=""
           className="flex-1 block w-full rounded-md border-2 py-2 px-1.5 bg-white text-gray-900 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6"
         >
