c6e329dee0b50f8f3b48a291d0188306259c6019
Noah Seethor <noahseethor@gmail.com>
2024-09-01 23:29:32 +0800

    Added Page Title Component File

---
+++ components/TextComponents/PageTitle.js
@@ -0,0 +1,8 @@
+export function PageTitle({ title, desc }) {
+  return (
+    <header className="w-full max-w-3xl mb-6">
+      <h1 className="text-3xl font-bold text-center text-gray-800">{title}</h1>
+      <p className="text-center text-gray-600 mt-2">{desc}</p>
+    </header>
+  );
+}
