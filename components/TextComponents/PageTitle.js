e73768c39ea5bd2d1be22674054859bb252e5883
keithxun <keithonghx@gmail.com>
2024-09-10 23:38:31 +0800

    Add condtional for desc rendering in page title

--- components/TextComponents/PageTitle.js
+++ components/TextComponents/PageTitle.js
@@ -2,7 +2,7 @@ export function PageTitle({ title, desc }) {
   return (
     <header className="w-full my-4">
       <h1 className="text-3xl font-bold text-center text-gray-800">{title}</h1>
-      <p className="text-center text-gray-600 mt-2">{desc}</p>
+      {desc && <p className="text-center text-gray-600 mt-2">{desc}</p>}
     </header>
   );
 }
