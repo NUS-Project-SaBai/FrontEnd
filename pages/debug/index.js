d81b8905ebeddc0c9a491df5bbcc5c13882bd4e3
keithxun <keithonghx@gmail.com>
2024-09-12 01:04:26 +0800

    Remove padding for the page title

--- pages/debug/index.js
+++ pages/debug/index.js
@@ -83,7 +83,7 @@ export default function DebuggingPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center">
       <PageTitle
         title="API Debugging Tool"
         desc="Use this tool to test and debug your API endpoints."
