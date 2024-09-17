5455b3aabedf638f1db1c4dad41c600f8810ebed
Poh Anson <50758680+PohAnson@users.noreply.github.com>
2024-09-17 21:39:43 +0800

    Bug Fixing

--- app/api/v1/[...apiurl]/route.js
+++ app/api/v1/[...apiurl]/route.js
@@ -1,47 +1,41 @@
 export async function GET(req) {
-  let apiurl = req.nextUrl.pathname.slice(8) + req.nextUrl.search;
-  console.log(req.method, apiurl);
-  const backendUrl = `http://127.0.0.1:8000/${apiurl}`;
-  const res = await fetch(backendUrl)
-    .then(async r => await r.json())
-    .catch(error => console.log(error));
+  const res = await get_delete(req);
   return Response.json(res);
 }
 
 export async function POST(req) {
-  let apiurl = req.nextUrl.pathname.slice(8) + req.nextUrl.search;
-
-  const backendUrl = `http://127.0.0.1:8000/${apiurl}`;
-  console.log(req.method, apiurl);
-  let body, res;
-  if (apiurl === 'patients') {
-    body = await req.formData();
-    res = await fetch(backendUrl, {
-      method: req.method,
-      body: body,
-    }).then(async r => await r.json());
-  } else {
-    body = await req.json();
-    res = await fetch(backendUrl, {
-      method: req.method,
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-        Cookie: req.cookie,
-        authorization: req.headers.authorization,
-      },
-    }).then(async r => await r.json());
-  }
+  const res = await patch_post(req);
   return Response.json(res);
 }
 
 export async function PATCH(req) {
-  let apiurl = req.nextUrl.pathname.slice(8) + req.nextUrl.search;
+  const res = await patch_post(req);
+  return Response.json(res);
+}
 
+export async function DELETE(req) {
+  const res = await get_delete(req);
+  return Response.json(res);
+}
+
+async function get_delete(req) {
+  let apiurl = req.nextUrl.pathname.slice(8) + req.nextUrl.search;
   const backendUrl = `http://127.0.0.1:8000/${apiurl}`;
   console.log(req.method, apiurl);
+
+  const res = await fetch(backendUrl, { method: req.method })
+    .then(async r => await r.json())
+    .catch(error => console.log(error));
+  return res;
+}
+
+async function patch_post(req) {
+  let apiurl = req.nextUrl.pathname.slice(8) + req.nextUrl.search;
+  const backendUrl = `http://127.0.0.1:8000/${apiurl}`;
+  console.log(req.method, apiurl);
+
   let body, res;
-  if (apiurl === 'patients') {
+  if (apiurl.split('/')[0] === 'patients') {
     body = await req.formData();
     res = await fetch(backendUrl, {
       method: req.method,
@@ -54,20 +48,9 @@ export async function PATCH(req) {
       body: JSON.stringify(body),
       headers: {
         'Content-Type': 'application/json',
-        Cookie: req.cookie,
-        authorization: req.headers.authorization,
+        Authorization: req.headers.get('authorization'),
       },
     }).then(async r => await r.json());
   }
-  return Response.json(res);
-}
-
-export async function DELETE(req) {
-  let apiurl = req.nextUrl.pathname.slice(8) + req.nextUrl.search;
-  const backendUrl = `http://127.0.0.1:8000/${apiurl}`;
-  console.log(req.method, apiurl);
-  const res = await fetch(backendUrl)
-    .then(async r => await r.json())
-    .catch(error => console.log(error));
-  return Response.json(res);
+  return res;
 }
