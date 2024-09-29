55cbe70e45d5cab1005af6bdeb5078f0f33eda1a
nathantew14 <nathantew14@gmail.com>
2024-09-29 23:44:50 +0800

    remove default chevron from select component

--- components/records/Header.js
+++ components/records/Header.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import toast from 'react-hot-toast';
 import moment from 'moment';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import { CLOUDINARY_URL } from '@/utils/constants';
 import { Button } from '../TextComponents';
 import axiosInstance from '@/pages/api/_axiosInstance';
@@ -83,10 +84,11 @@ export function Header({ patient, visits, handleVisitChange }) {
             <select
               name="medication"
               onChange={handleVisitChange}
-              className="w-full bg-white border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500"
+              className="w-full bg-white border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500 appearance-none"
             >
               {visitOptions}
             </select>
+            <ChevronDownIcon className="absolute inset-y-0 right-0 h-5 w-5 text-gray-700 pointer-events-none my-auto mr-2" />
           </div>
         </div>
       </div>
