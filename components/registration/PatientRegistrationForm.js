c92919c8c9040414db3a54e145e2d3a5c25427f6
nathantew14 <nathantew14@gmail.com>
2024-11-05 17:30:48 +0800

    remove copied stuff after merging

--- components/registration/PatientRegistrationForm.js
+++ components/registration/PatientRegistrationForm.js
@@ -1,57 +1,7 @@
 import { Button, InputField, InputBox } from '@/components/TextComponents';
 import AppWebcam from '@/components/WebCamera';
 import React, { useState } from 'react';
-// import { VenueOptionsDropdown } from './VenueOptionsDropdown';
-
-export const VenueOptionsDropdown = ({ handleInputChange }) => {
-  const {
-    field: { value, onChange },
-    fieldState: { error },
-  } = useController({
-    name: 'village_prefix',
-    rules: { required: 'Village is required' },
-  });
-
-  const combinedOnChange = e => {
-    handleInputChange(e);
-    onChange(e);
-  };
-
-  return (
-    <div>
-      <label
-        htmlFor="village_prefix"
-        className="block text-sm font-medium leading-6 text-gray-900"
-      >
-        Village
-        <span className="text-red-500">*</span> {/* indicate required */}
-      </label>
-      <div className="mt-1 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-400">
-        <select
-          name="village_prefix"
-          id="village_prefix"
-          onChange={combinedOnChange}
-          value={value}
-          className={`flex-1 block w-full rounded-md border-2 py-2 px-1.5 bg-white focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm sm:leading-6 ${VILLAGE_COLOR_CLASSES[value] || 'text-gray-500'}`}
-        >
-          <option hidden value="">
-            Please select an option
-          </option>
-          {Object.entries(VENUE_OPTIONS).map(([key, value]) => (
-            <option
-              className={`${VILLAGE_COLOR_CLASSES[key] || 'text-gray-500'}`}
-              value={key}
-              key={key}
-            >
-              {value}
-            </option>
-          ))}
-        </select>
-      </div>
-      {error && <p className="text-red-500 text-sm">{error.message}</p>}
-    </div>
-  );
-};
+import { VenueOptionsDropdown } from './VenueOptionsDropdown';
 
 export function PatientRegistrationForm({
   formDetails,
