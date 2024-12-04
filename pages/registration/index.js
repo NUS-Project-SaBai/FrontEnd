e80827c8352bdd0a3c85f742bdb87b242896cb40
Poh Anson <50758680+PohAnson@users.noreply.github.com>
2024-12-04 12:47:57 +0800

    Make date of birth compulsory

--- pages/registration/index.js
+++ pages/registration/index.js
@@ -91,16 +91,13 @@ const Registration = () => {
       'bs2',
       'sabai',
     ];
-
+    console.log(formDetails);
     if (formDetails.name == '') {
       toast.error('Name cannot be empty.');
       return;
     }
 
-    if (
-      formDetails['date_of_birth'].length !== 10 &&
-      formDetails['date_of_birth'].length !== 0
-    ) {
+    if (formDetails['date_of_birth'].length !== 10) {
       toast.error('Please enter a valid date of birth!');
       return;
     }
