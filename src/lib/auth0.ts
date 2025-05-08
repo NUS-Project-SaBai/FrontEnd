fac1d1d607141c91b6b739a974097467677e623b
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-05-08 23:47:26 +0800

    fix: testing deployment

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,7 +1,5 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-process.env.APP_BASE_URL = process.env.APP_BASE_URL || process.env.VERCEL_URL;
-console.log(process.env.APP_BASE_URL);
 export const auth0 = new Auth0Client({
   authorizationParameters: { audience: 'https://sabai.jp.auth0.com/api/v2/' },
 });
