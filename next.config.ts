54d8809726bcb3fe212b45ff917d38c234853c00
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-05-11 23:52:16 +0800

    debugging

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: 'res.cloudinary.com' }],
   },
+  env: {
+    APP_BASE_URL: process.env.VERCEL
+      ? `https://${process.env.VERCEL_URL}`
+      : process.env.APP_BASE_URL,
+  },
 };
 
 export default nextConfig;
--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,7 +1,5 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-process.env.APP_BASE_URL =
-  process.env.APP_BASE_URL || 'https://' + process.env.VERCEL_URL;
 export const auth0 = new Auth0Client({
   authorizationParameters: { audience: 'https://sabai.jp.auth0.com/api/v2/' },
 });
