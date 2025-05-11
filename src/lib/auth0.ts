324eb2a203a20ad490297774ee10d9348a5e0b97
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-05-11 22:56:12 +0800

    debugging

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -2,4 +2,8 @@ import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 export const auth0 = new Auth0Client({
   authorizationParameters: { audience: 'https://sabai.jp.auth0.com/api/v2/' },
+  appBaseUrl:
+    process.env.APP_BASE_URL == undefined
+      ? 'https://' + process.env.VERCEL_URL
+      : process.env.APP_BASE_URL,
 });
