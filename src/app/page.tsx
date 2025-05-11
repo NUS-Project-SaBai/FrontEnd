21b3549fe7f62fc7a8f3d59ea68d87d143063bce
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-05-11 22:11:12 +0800

    debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,5 @@ import { useUser } from '@auth0/nextjs-auth0';
 
 export default function Home() {
   const { user } = useUser();
-  console.log(
-    'appBaseUrl:',
-    process.env.VERCEL_URL,
-    process.env.APP_BASE_URL,
-    process.env.VERCEL_URL || process.env.APP_BASE_URL
-  );
   return <p>Hi {user?.nickname}</p>;
 }
