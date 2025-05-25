2de21a6687a345b3572b29bf75598ede3689e0bf
Ng Tze Han, Benedict <E1406493@u.nus.edu>
2025-05-25 17:03:16 +0800

    added a sticky header for the search bar and table header

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { LoadingPage } from '@/components/LoadingPage';
-import { LoadingUI } from '@/components/LoadingUI';
 import { PatientSearchInput } from '@/components/PatientSearchbar';
 import { useLoadingState } from '@/hooks/useLoadingState';
 import { Patient } from '@/types/Patient';
@@ -24,6 +22,17 @@ export default function RecordPage() {
       <LoadingPage isLoading={isLoading} message="Loading Patients...">
         <PatientRecordTable patients={patients} />
       </LoadingPage>
+    <div className="flex h-screen flex-col p-2 pb-5">
+      {/* Sticky header for the search input */}
+      <div className="z-1 sticky left-0 top-0 w-full bg-white pb-2">
+        <h1>Patients List test</h1>
+        <Suspense>
+          <PatientSearchInput setPatients={setPatients} />
+        </Suspense>
+      </div>
+      <div className="flex-1">
+        <PatientRecordTable patients={patients} />
+      </div>
     </div>
   );
 }
