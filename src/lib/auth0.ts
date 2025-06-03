2ad83bfcf748bbddddb17cd87b28097465ed6a27
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-06-03 21:48:24 +0800

    Fix Upload File Size Limit
    * Added axiosClientInstance
    * In RHFInputField, so long as the input is a file, check that it does not exceed 50MB

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,5 +1,11 @@
+import { APP_CONFIG } from '@/config';
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
 export const auth0 = new Auth0Client({
   authorizationParameters: { audience: 'https://sabai.jp.auth0.com/api/v2/' },
+  appBaseUrl: APP_CONFIG.APP_BASE_URL,
+  secret: process.env.AUTH0_SECRET,
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
 });
