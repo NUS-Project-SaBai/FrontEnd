d7663ff56c13b0c56290b46ad6e00a462420a096
Ng Tze Han, Benedict <E1406493@u.nus.edu>
2025-06-06 16:38:41 +0800

    updated sticky header

--- src/app/records/page.tsx
+++ src/app/records/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { LoadingPage } from '@/components/LoadingPage';
+import { LoadingUI } from '@/components/LoadingUI';
 import { PatientSearchInput } from '@/components/PatientSearchbar';
 import { useLoadingState } from '@/hooks/useLoadingState';
 import { Patient } from '@/types/Patient';
@@ -11,28 +13,19 @@ export default function RecordPage() {
 
   return (
     <div className="p-2">
-      <h1>Patients List</h1>
-      <Suspense fallback={<LoadingUI message="Loading search input..." />}>
-        <PatientSearchInput
-          setPatients={setPatients}
-          isLoading={isLoading}
-          withLoading={withLoading}
-        />
-      </Suspense>
+      <div className="z-1 sticky top-0 w-full bg-white">
+        <h1>Patients List</h1>
+        <Suspense fallback={<LoadingUI message="Loading search input..." />}>
+          <PatientSearchInput
+            setPatients={setPatients}
+            isLoading={isLoading}
+            withLoading={withLoading}
+          />
+        </Suspense>
+      </div>
       <LoadingPage isLoading={isLoading} message="Loading Patients...">
         <PatientRecordTable patients={patients} />
       </LoadingPage>
-    <div className="flex h-screen flex-col p-2 pb-5">
-      {/* Sticky header for the search input */}
-      <div className="z-1 sticky left-0 top-0 w-full bg-white pb-2">
-        <h1>Patients List test</h1>
-        <Suspense>
-          <PatientSearchInput setPatients={setPatients} />
-        </Suspense>
-      </div>
-      <div className="flex-1">
-        <PatientRecordTable patients={patients} />
-      </div>
     </div>
   );
 }
