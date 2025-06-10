da07850e44f3dfa415de58c3160f4a25cf7ecaf8
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-06-10 18:02:06 +0800

    Proper Loading
    - Proper loading when updating the state

--- src/app/referrals/page.tsx
+++ src/app/referrals/page.tsx
@@ -1,13 +1,9 @@
 'use client';
-import { LoadingPage } from '@/components/LoadingPage';
+import { LoadingUI } from '@/components/LoadingUI';
 import { PatientSearchInputByReferral } from '@/components/referrals/PatientSearchbarByReferral';
-import {
-  getReferrals,
-  ReferralWithDetails,
-} from '@/data/referrals/getReferrals';
+import { ReferralWithDetails } from '@/data/referrals/getReferrals';
 import { useLoadingState } from '@/hooks/useLoadingState';
-import { Patient } from '@/types/Patient';
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useState } from 'react';
 import ReferralCard from './ReferralCard';
 
 export default function ReferralPage() {
@@ -15,55 +11,43 @@ export default function ReferralPage() {
     ReferralWithDetails[]
   >([]);
   const { isLoading, withLoading } = useLoadingState(true);
-  const [patients, setPatients] = useState<Patient[]>([]);
-
-  useEffect(() => {
-    const patientsTemp: Patient[] = [];
-    const fetchReferrals = withLoading(async () => {
-      const data = await getReferrals();
-      setReferralsWithPatient(data);
-
-      referralWithDetails.forEach(item => patients.push(item.patient));
-      setPatients(patientsTemp);
-    });
-    fetchReferrals();
-  }, []);
 
   return (
-    <LoadingPage isLoading={isLoading} message="Loading referrals...">
-      <div className="p-2">
-        <h1>Referrals</h1>
-        <Suspense>
-          <PatientSearchInputByReferral
-            setReferrals={setReferralsWithPatient}
-          />
-        </Suspense>
-
-        {referralWithDetails.length == 0 ? (
-          <h2>No referrals</h2>
-        ) : (
-          <table className="w-full divide-y-2 divide-gray-500">
-            <thead>
-              <tr>
-                <th>Photo</th>
-                <th>Referral Summary</th>
-                <th>To details</th>
-                <th>Referral Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {referralWithDetails.map(ref => (
-                <ReferralCard
-                  key={ref.referral.id}
-                  ref={ref.referral}
-                  patient={ref.patient}
-                  date={ref.date}
-                />
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-    </LoadingPage>
+    <div className="p-2">
+      <h1>Referrals</h1>
+      <Suspense>
+        <PatientSearchInputByReferral
+          isLoading={isLoading}
+          withLoading={withLoading}
+          setReferrals={setReferralsWithPatient}
+        />
+      </Suspense>
+      {isLoading ? (
+        <LoadingUI message="Loading referrals..." />
+      ) : referralWithDetails.length == 0 ? (
+        <h2>No referrals</h2>
+      ) : (
+        <table className="w-full divide-y-2 divide-gray-500">
+          <thead>
+            <tr>
+              <th>Photo</th>
+              <th>Referral Summary</th>
+              <th>To details</th>
+              <th>Referral Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {referralWithDetails.map(ref => (
+              <ReferralCard
+                key={ref.referral.id}
+                ref={ref.referral}
+                patient={ref.patient}
+                date={ref.date}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 }
