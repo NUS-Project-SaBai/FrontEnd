ab7b5ab511e4d526a085e5c259be5d0bae636fc2
nathantew14 <nathantew14@gmail.com>
2025-06-24 18:27:30 +0800

    removed comments

--- src/app/records/patient-record/page.tsx
+++ src/app/records/patient-record/page.tsx
@@ -8,7 +8,6 @@ export default async function PatientRecordPage({
 }: {
   searchParams: Promise<{ id: string; visit: string }>;
 }) {
-  // const { id: patientId = '', visit: visitId = '' } = await searchParams;
   const { id: patientId = '' } = await searchParams;
 
   const patient = await getPatientById(patientId);
@@ -25,32 +24,9 @@ export default async function PatientRecordPage({
   return (
     <div className="p-2">
       <h1>Patient Records</h1>
-
       <PatientInfoHeaderSection patient={patient} />
       <hr className="my-2 w-full border-t-2" />
       <PatientRecordBody patient={patient} />
-      {/* <div className="grid grid-cols-2 gap-1 md:gap-4">
-        {visits == null ? (
-          <div className="w-fit text-nowrap text-lg">
-            <LoadingUI message="Loading Visits..." />
-          </div>
-        ) : visits.length == 0 ? (
-          <div>
-            <p>No Visits Found</p>
-          </div>
-        ) : (
-          <div>
-            <VisitDropdown name="visit_date" visits={visits} />
-          </div>
-        )}
-        <ViewVital
-          consults={consults || []}
-          patient={patient}
-          vitals={vitals}
-        />
-        <EditPatient patient={patient} />
-        <PrescriptionConsultCol consults={consults} />
-      </div> */}
     </div>
   );
 }
