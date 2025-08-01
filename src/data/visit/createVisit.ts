288e1ea9a53b1a715faf1e912c494d9c0d060d23
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-08-01 22:34:30 +0800

    Implement useSaveOnWrite for ConsultationForm
    Fix the data required by createVisit

--- src/data/visit/createVisit.ts
+++ src/data/visit/createVisit.ts
@@ -4,9 +4,8 @@ import { Patient } from '@/types/Patient';
 import moment from 'moment';
 
 export async function createVisit(patient: Patient) {
-  console.log('CREATING VISIT');
-  await axiosInstance.post(`/visits/?patient=${patient.pk}`, {
-    patient: patient.pk,
+  await axiosInstance.post(`/visits/`, {
+    patient_id: patient.pk,
     visit_date: moment().format('DD MMMM YYYY HH:mm'),
     status: 'started',
   });
