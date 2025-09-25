6267884bc8c0e83c88fcec12a97b387c08f6e91a
Derrick Low <lowderrickym@gmail.com>
2025-09-25 23:32:36 +0800

    hide referral notes when not referred option is selected from the dropdown in consultation form

--- src/components/records/consultation/ConsultationForm.tsx
+++ src/components/records/consultation/ConsultationForm.tsx
@@ -228,9 +228,7 @@ export function ConsultationForm({
               label="Referral Notes"
               type="textarea"
               placeholder="Type your referral notes here..."
-              isRequired={
-                useFormReturn.watch('referred_for') !== 'Not Referred'
-              }
+              isRequired={useFormReturn.watch('referred_for') !== ''}
             />
           )}
 
