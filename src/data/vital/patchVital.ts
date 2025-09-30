e52423a6473ccfc4fa075314ba04d62390cd621b
PohAnson <50758680+pohanson@users.noreply.github.com>
2025-09-30 10:38:13 +0800

    Fix updating vital problem

--- src/data/vital/patchVital.ts
+++ src/data/vital/patchVital.ts
@@ -6,7 +6,7 @@ import { Vital } from '@/types/Vital';
 export async function patchVital(vital: Vital) {
   // remove empty fields to reduce data sent
   const jsonPayload = Object.fromEntries(
-    Object.entries(vital).filter(([, v]) => v != undefined || v != '')
+    Object.entries(vital).filter(([, v]) => v != undefined && v != '')
   );
   try {
     return (
