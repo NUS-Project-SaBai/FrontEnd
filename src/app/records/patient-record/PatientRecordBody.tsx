51b9571fada72c2bdbaea45cfb20f57781a70430
nathantew14 <nathantew14@gmail.com>
2025-10-10 17:01:32 +0800

    Add back my patient record header

--- src/app/records/patient-record/PatientRecordBody.tsx
+++ src/app/records/patient-record/PatientRecordBody.tsx
@@ -29,7 +29,9 @@ export async function PatientRecordBody({ patient }: { patient: Patient }) {
           const thisVisitConsults = consults?.filter(
             v => v.visit.id == visit.id
           );
-          const vital = vitals?.find(v => v.visit?.id == visit.id) ?? null;
+          console.log(vitals);
+          const vital =
+            vitals?.find(vital => vital.visit_id == visit.id) ?? null;
 
           const isLatestVisit = visit.id == patient.last_visit_id;
 
--- src/components/records/patient/PatientInfoHeaderSection.tsx
+++ src/components/records/patient/PatientInfoHeaderSection.tsx
@@ -1,28 +1,16 @@
 'use client';
-import { LoadingUI } from '@/components/LoadingUI';
 import { PatientPhoto } from '@/components/PatientPhoto';
 import { UploadDocument } from '@/components/records/UploadDocument';
 import { ViewDocument } from '@/components/records/ViewDocument';
-import { VisitDropdown } from '@/components/VisitDropdown';
 import { VILLAGES_AND_ALL } from '@/constants';
 import { getUploadByPatientId } from '@/data/fileUpload/getUpload';
-import { getVisitsByPatientId } from '@/data/visit/getVisit';
-import { WithLoadingType } from '@/hooks/useLoadingState';
-import { Patient, getPatientAge } from '@/types/Patient';
+import { Patient } from '@/types/Patient';
 import { Upload } from '@/types/Upload';
-import { Visit } from '@/types/Visit';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { EditPatient } from './EditPatient';
+import { PatientDetails } from './PatientDetails';
 
-export function PatientInfoHeaderSection({
-  patient,
-  withLoading = x => x,
-  showVisit = true,
-}: {
-  patient: Patient;
-  withLoading?: WithLoadingType;
-  showVisit?: boolean;
-}) {
-  const [visits, setVisits] = useState<Visit[] | null>(null);
+export function PatientInfoHeaderSection({ patient }: { patient: Patient }) {
   const [documents, setDocuments] = useState<Upload[]>([]);
 
   const fetchDocuments = () => {
@@ -31,68 +19,32 @@ export function PatientInfoHeaderSection({
     });
   };
 
-  useEffect(() => {
-    if (showVisit) {
-      withLoading(getVisitsByPatientId)(patient.pk.toString()).then(vs =>
-        setVisits(vs)
-      );
-    }
-    fetchDocuments();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [patient.pk]);
-
-  const age = getPatientAge(patient);
-
   return (
     <div className="flex">
-      <PatientPhoto pictureUrl={patient.picture_url} width={180} height={180} />
-      <div className="grid grid-cols-[2fr,3fr] grid-rows-2 gap-x-8 pl-8 text-2xl">
-        <div>
-          <p>ID:</p>
-          <p
-            className={
-              'font-bold ' + VILLAGES_AND_ALL[patient.village_prefix].color
-            }
-          >
-            {patient.patient_id}
-          </p>
-        </div>
-
-        <div>
-          <p>Name:</p>
-          <p>{patient.name}</p>
-        </div>
-
-        {showVisit &&
-          (visits == null ? (
-            <div className="w-fit text-nowrap text-lg">
-              <LoadingUI message="Loading Visits..." />
-            </div>
-          ) : visits.length == 0 ? (
-            <div>
-              <p>No Visits Found</p>
-            </div>
-          ) : (
-            <div>
-              <VisitDropdown name="visit_date" visits={visits} />
-            </div>
-          ))}
-
-        <div>
-          <p>Age:</p>
-          <p className="text-lg">
-            <span className="font-bold">{age.year}</span>
-            <span> YEARS </span>
-            <span className="font-bold">{age.month}</span>
-            <span> MONTHS </span>
-            <span className="font-bold">{age.day}</span>
-            <span> DAYS </span>
-          </p>
-        </div>
+      <div className="relative h-[15vw] w-[15vw]">
+        <PatientPhoto
+          pictureUrl={patient.picture_url}
+          width={180}
+          height={180}
+        />
       </div>
-      <div className="flex flex-col space-y-2">
-        <UploadDocument patient={patient} onUploadSuccess={fetchDocuments} />
-        <ViewDocument documents={documents} setDocuments={setDocuments} />
+      <div className="flex flex-1 flex-col">
+        <div className="m-2 flex flex-row gap-2">
+          <h1 className="flex-1 content-center text-start">
+            <span
+              className={
+                'font-bold ' + VILLAGES_AND_ALL[patient.village_prefix].color
+              }
+            >
+              {patient.patient_id}
+            </span>
+            <span>, {patient.name}</span>
+          </h1>
+          <UploadDocument patient={patient} onUploadSuccess={fetchDocuments} />
+          <ViewDocument documents={documents} setDocuments={setDocuments} />
+          <EditPatient patient={patient} />
+        </div>
+        <PatientDetails patient={patient} />
       </div>
     </div>
   );
