32662c157948a532b18121cc4b452d792efb316b
Keiran <keinn2210@gmail.com>
2025-12-01 17:50:20 +0800

    Update image remote patterns and modify offline user authentication logic for offline use

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ hostname: 'res.cloudinary.com' }],
+    remotePatterns: [{ hostname: 'res.cloudinary.com' }, { hostname: "192.168.0.200"}],
   },
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -23,7 +23,7 @@ export async function middleware(req: NextRequest) {
   const offlineUser = req.cookies.get('offlineUser');
 
   // if user is not authenticated, redirect them to login.
-  if (APP_CONFIG.OFFLINE && offlineUser == null) {
+  if (APP_CONFIG.OFFLINE && offlineUser == null && false) { // TODO: offline login page
     return NextResponse.redirect(new URL('/login', req.url));
   } else if (!APP_CONFIG.OFFLINE && !session) {
     return NextResponse.redirect(`${origin}/auth/login`);
