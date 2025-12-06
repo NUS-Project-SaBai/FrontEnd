ebbfca998dec47094e995922e0a0418c53d7dbca
nathantew14 <nathantew14@gmail.com>
2025-12-07 01:07:01 +0700

    Improve UI

--- src/app/vision/add-update-glasses/page.tsx
+++ src/app/vision/add-update-glasses/page.tsx
@@ -45,7 +45,7 @@ export default async function PatientVisionPage({
   ]);
 
   return (
-    <div className="p-2">
+    <div className="p-2 flex flex-col gap-2">
       <h1>Patient Glasses Details</h1>
 
       {/* Patient info */}
